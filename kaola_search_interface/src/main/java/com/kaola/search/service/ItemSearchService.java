05b186a9bc52b21c47aaa4dd88e18a02296ecb3a
13781013522 <1435253712@qq.com>
2020-03-22 16:43:05 +0800

    考拉项目整合git

---
+++ kaola_search_interface/src/main/java/com/kaola/search/service/ItemSearchService.java
@@ -0,0 +1,13 @@
+package com.kaola.search.service;
+
+import java.util.Map;
+
+public interface ItemSearchService {
+    /**
+     * 搜索
+     * @param
+     * @return
+     */
+    public Map<String,Object> search(Map searchMap);
+
+}
